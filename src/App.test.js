2852607fad8fb5ebaedf5742a49e47676faa3fb7
Hamza Es-saady <es.saady.hamza@gmail.com>
2023-10-17 17:22:34 +0100

    feat: add test and implementation of color button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has initial text and color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+  expect(colorButton).toHaveStyle({ "background-color": "red" });
+});
+test("button changes color and text when clicked, reverts to initial color and text when re-clicked", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ "background-color": "blue" });
+  expect(colorButton).toHaveTextContent(/change to red/i);
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ "background-color": "red" });
+  expect(colorButton).toHaveTextContent(/change to blue/i);
 });
